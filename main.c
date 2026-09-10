3276cad7bd5069644dd1bc6859fe2e96fa58d888
ojaogomess <05796355031@senacrs.edu.br>
2026-09-09 21:58:15 -0300

    nova biblioteca (<stdbool.h> boolehPar

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include<stdio.h>
 #include<windows.h>
+#include<stdbool.h>
+
+    bool ehPar(int numero){
+        return(numero % 2 == 0);
+    }
 
 int main () {
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a = 5;
-   int b = 4;
-
-   printf("O maior número é: %d\n", (a + b));
-    printf("Divisão de 5 por 4 é: %d\n", (a/b));
-    printf("A multiplicação de 5 por 4 é: %d\n", (a * b));
-    printf("A subtração é: %d\n", (a-b));
-
+    bool resultado1 = ehPar(4);
+    bool resultado2 = ehPar(7);
+    bool resultado3 = ehPar(20);
+    bool resultado4 = ehPar(144);
+    bool resultado5 = ehPar(200);
+    
+    printf("4 é par? %d\n", resultado1);
+    printf("7 é par? %d\n", resultado2);
+    printf("20 é par? %d\n", resultado3);
+    printf("144 é par? %d\n", resultado4);
+    printf("200 é par? %d\n", resultado5);
 
 
     return 0;
