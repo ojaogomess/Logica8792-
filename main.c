5fa5c2a355307894fe941c61ad649df8669d1953
ojaogomess <05796355031@senacrs.edu.br>
2026-09-09 21:18:36 -0300

    adição, multiplicação, subtração e divisão nova biblioteca

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ int main () {
    int a = 5;
    int b = 4;
 
-   printf("O maior numero e: %d\n", (a + b));
-    printf("Divisao de 5 por 4 é: %d\n", (a/b));
-    printf("A multiplicacao de 5 por 4 e: %d\n", (a * b));
-    printf("A subtracao e: %d\n", (a-b));
+   printf("O maior número é: %d\n", (a + b));
+    printf("Divisão de 5 por 4 é: %d\n", (a/b));
+    printf("A multiplicação de 5 por 4 é: %d\n", (a * b));
+    printf("A subtração é: %d\n", (a-b));
 
 
 
