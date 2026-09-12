a965a4acfb6e89a13e1f28f5d7a56e6818fa4afd
ojaogomess <05796355031@senacrs.edu.br>
2026-09-11 21:16:46 -0300

    media usando apenas um scan e print

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 
 int main () {
 
-    float raio;
-    const float PI = 3.14159;
-
-    printf("Digite o raio do circulo: ");
-    scanf("%f", &raio);
-    printf("Area: %.2f\n", PI * raio * raio);
-    printf("Perimetro: %.2f\n", 2 * PI * raio);
-
+    float n1, n2, n3, media;
    
+    printf("Digite três notas: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+   
+   media = (n1 + n2 + n3) / 3;
+   
+   printf("Media: %.2f\n", media);
+
     return 0;
 }
