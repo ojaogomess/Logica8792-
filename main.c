6f2477af3b98720bf151c349ed81c116eed6f9bc
ojaogomess <05796355031@senacrs.edu.br>
2026-09-16 20:14:12 -0300

    dias da semana

--- main.c
+++ main.c
@@ -7,30 +7,28 @@ int main () {
   SetConsoleCP(65001);
   SetConsoleOutputCP(65001);
 
-int n1, n2, n3;
-float media;
-float freq;
+    int diaDaSemana;
 
-printf("Digite a nota 1");
-scanf("%d", &n1);
+    printf("Digite um número de 1 a 7: ");
+    scanf("%d", &diaDaSemana);
 
-printf("Digite a nota 2");
-scanf("%d", &n2);
-
-printf("Digite a nota 3");
-scanf("%d", &n3);
-
-media = (n1 + n2 + n3) / 3;
-
-printf("digite a frequencia: ");
-scanf("%f", &freq);
-
-if(media >= 7 && freq >= 75){
-  printf("Aluno aprovado");
-}else{
-  printf("\n Aluno reprovado");
-}
-
+    if (diaDaSemana == 1){
+    printf("Domingo!");
+    }else if(diaDaSemana == 2){
+    printf("Segunda-feira!");
+    }else if(diaDaSemana == 3){
+      printf("Terça-Feira");
+    }else if(diaDaSemana == 4){
+      printf("Quarta-Feira");
+    }else if (diaDaSemana == 5){
+      printf ("Quinta-Feira");
+    }else if (diaDaSemana == 6){
+      printf("Sexta-Feira");
+    }else if (diaDaSemana == 7){
+      printf("Sábado");
+    }else{
+      printf("Valor invalido");
+    }   
 
 
 return 0;
