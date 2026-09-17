cb7b34132fe4488eff2c5a051ed635ddfa68f51b
ojaogomess <05796355031@senacrs.edu.br>
2026-09-16 21:28:22 -0300

    digite a nota: recup, aprov, reprov(if, else if, else) com LOCALE

--- main.c
+++ main.c
@@ -1,25 +1,34 @@
 #include<stdio.h>
+#include<locale.h>
 #include<windows.h>
-
 
 int main () {
 
   SetConsoleCP(65001);
   SetConsoleOutputCP(65001);
 
-   int nota;
+  setlocale(LC_ALL, "pt_BR.UTF-8");
 
-  printf("Digite a nota");
-  scanf("%d", &nota);
+  float n1, n2, n3, media;
 
-  if (nota >= 7){
-    printf("Aprovado!");
-  }else if(nota >= 5){
-    printf("Recuperação!");
-  }else{
-    printf("Reprovado!");
-  }
+  printf("Digite a nota 1: ");
+  scanf("%f", &n1);
 
+   printf("Digite a nota 2: ");
+  scanf("%f", &n2);
+
+ printf("Digite a nota 3: ");
+  scanf("%f", &n3);
+
+media = (n1 + n2 + n3) / 3;
+
+if(media >= 7){
+  printf("Aprovado");
+}else if(media >= 5){
+  printf("Recuperação");
+}else{
+  printf("Reprovado");
+}
 
 return 0;
 
