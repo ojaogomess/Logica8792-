034563926a461694cdf0a111b19c070b4a93b020
ojaogomess <05796355031@senacrs.edu.br>
2026-09-18 19:22:14 -0300

    sistema de pedagio usando switch+

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 
 int main(){
 
+SetConsoleCP(65001);
+SetConsoleOutputCP(65001);
+
 int opcao;
 
-printf("--- sistema de pedágio ---");
-printf("1 - Moto");
-printf("2 - Carro Passeio\n");
+printf("--- sistema de pedágio ---\n");
+printf("1 --- Moto ---\n");
+printf("2 --- Carro Passeio ---\n");
+printf("3 --- Caminhão ---\n");
+printf("4 --- Onibus ---\n");
 scanf("%d", &opcao);
 
 switch (opcao)
@@ -17,6 +22,13 @@ switch (opcao)
   break;
   case 2:
   printf("categoria: Carro Passeio | Tarifa R$ 10,00\n");
+  break;
+  case 3:
+  printf("Categoria: Caminhão | Tarifa: R$25,00\n");
+  break;
+  case 4:
+  printf("Categoria Onibus | Tarifa R$30,00\n");
+  break;
   default:
   printf("Erro: Categoria não adestrada no sistema.\n");
   break;
