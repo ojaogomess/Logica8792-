093316f6cc61795a6a938e54ec87d9e7974ef033
ojaogomess <05796355031@senacrs.edu.br>
2026-09-21 19:43:41 -0300

    For funfando com 10000

--- main.c
+++ main.c
@@ -6,26 +6,13 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-
-float peso, altura, imc;
-
-printf("Digite a altura (m): ");
-scanf("%f", &altura);
-
-printf("Digite o peso (kg): ");
-scanf("%f", &peso);
-
-imc = peso / (altura * altura);
-
-if(imc < 18.5){
-  printf("Classificação: Abaixo do peso\n");
-}else if(imc < 25){
-  printf("Classificação: Peso normal!\n");
-}else if(imc < 30){
-  printf("Classificação: Acima do peso");
-}else{
-  printf("Classificação: Obesidade");
+for(int i = 0; i < 10000; i++){
+  printf("%d\n", i);
 }
+
+printf("\nFor funfando");
+
+
 
 
 return 0;
