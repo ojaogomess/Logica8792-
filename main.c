1be99b3542ce5682a8f2dcfcc1cbadd301abd6ca
ojaogomess <05796355031@senacrs.edu.br>
2026-09-21 20:54:35 -0300

    Numeros floats, carros e numeros inteiros usando array

--- main.c
+++ main.c
@@ -6,17 +6,29 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-char nomes[7][20] = {"João\n", "Ana\n", "Melissa\n", "Marcelo\n", "Deisi\n", "Meiriane\n", "Cleber\n"};
+int numerosInteiros[5] = {1, 5, 10, 15, 20};
+float numerosFloats[5] = {10.5, 20.8, 30.2, 40.9, 50.1}; 
+char carros[3][20] = {"Ferrari", "Mercedez", "Volkswagen"};
 
-printf("%s", nomes[0]);
-printf("%s", nomes[1]);
-printf("%s", nomes[2]);
-printf("%s", nomes[3]);
-printf("%s", nomes[4]);
-printf("%s", nomes[5]);
-printf("%s", nomes[6]);
+printf("\n");
+
+printf("%s\n", carros[0]);
+printf("%s\n", carros[1]);
+printf("%s\n", carros[2]);
+
+printf("\n------------------\n");
+
+printf("\n%.1f\n", numerosFloats[0]);
+printf("%.1f\n", numerosFloats[1]);
+printf("%.1f\n", numerosFloats[2]);
+printf("%.1f\n", numerosFloats[3]);
+printf("%.1f\n", numerosFloats[4]);
+
+printf("\n-------------------\n");
+
+printf("\n%d\n%d\n%d\n%d\n%d\n", numerosInteiros[0], numerosInteiros[1], numerosInteiros[2], numerosInteiros[3], numerosInteiros[4]);
+
 
 
 return 0;
-
 }
