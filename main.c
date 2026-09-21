f8c8fa4bd1f469787354ac8fbe00e21529674234
ojaogomess <05796355031@senacrs.edu.br>
2026-09-21 20:04:41 -0300

    Array nomes[7][20]

--- main.c
+++ main.c
@@ -6,12 +6,15 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-char nomes[3][20] = {"João\n", "Ana\n", "Melissa\n"};
+char nomes[7][20] = {"João\n", "Ana\n", "Melissa\n", "Marcelo\n", "Deisi\n", "Meiriane\n", "Cleber\n"};
 
 printf("%s", nomes[0]);
 printf("%s", nomes[1]);
 printf("%s", nomes[2]);
-
+printf("%s", nomes[3]);
+printf("%s", nomes[4]);
+printf("%s", nomes[5]);
+printf("%s", nomes[6]);
 
 
 return 0;
