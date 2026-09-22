413f2e32a0c13837c5bb0971f26e0f79e25cf9a2
ojaogomess <05796355031@senacrs.edu.br>
2026-09-22 19:28:49 -0300

    funcao

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 #include<windows.h>
 
+void dobrar(int *x){
+    *x = *x * 2;
+}
+
+
 int main(){
 
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int x = 10;
-int *p = &x;
 
-printf("Valor de x: %d\n", x);
-printf("Endereço de x: %p\n", &x);
-printf("Valor via ponteiro: %d\n", *p);
+int numero = 10;
+int n1 = 50;
+
+dobrar(&numero);
+printf("Número depois da função %d\n", numero);
+
 
 
 
