1b65073867751f4ff635a8362688e81d3bac358c
ojaogomess <05796355031@senacrs.edu.br>
2026-09-21 21:09:57 -0300

    for i <= 10

--- main.c
+++ main.c
@@ -6,12 +6,12 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int idade[3] = {25, 35, 45};
 
-printf("%s %d %s", "João tem:",idade[0], "anos!\n");
-printf("%s %d %s", "Ana tem:",idade[1],"anos!\n");
-printf("%s %d %s", "Marcelo tem",idade[2],"anos!");
 
+
+for(int i = 0; i <= 10; i = i + 2){
+  printf("%d\n", i);
+}
 
 
 return 0;
