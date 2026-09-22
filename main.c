c76d53e90f9a326d6c7bafd8e86d3e631f5a5a1c
ojaogomess <05796355031@senacrs.edu.br>
2026-09-22 20:49:01 -0300

    funcao int subtrair, somar e divdir

--- main.c
+++ main.c
@@ -10,11 +10,20 @@ int somar(int a, int b){
     return a + b;   
 }
 
-
+int dividir(int a, int b){
+    if(b <= 0){
+        printf("Ele precisa ser maior que 0!");
+    }else{
+        return a / b;
+    }
+}
 int main(){
 
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
+
+
+
 
 
 int resultado = somar( 5, 3);
@@ -22,6 +31,9 @@ printf("O resultado da soma é: %d", resultado);
 
 int resultado2 = subtrair(5, 3);
 printf("\nO resultado da subtração é: %d", resultado2);
+
+int resultado3 = dividir(10, 5);
+printf("\n o resultado da divisão é %d", resultado3);
 
 return 0;
 }
