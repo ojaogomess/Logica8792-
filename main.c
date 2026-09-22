d973835335c084a55e9ba3d73522c52e40811a23
ojaogomess <05796355031@senacrs.edu.br>
2026-09-22 20:12:52 -0300

    funcao int subtrair e somar

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 #include<windows.h>
 
-void saudar(const char *nome){ //nao tem parametro ()
-    printf("Olá %s\n", nome);
-    
+int subtrair(int a, int b){
+    return a - b;
+}
+
+
+int somar(int a, int b){
+    return a + b;   
 }
 
 
@@ -12,9 +16,12 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-saudar("João Vitor");
-saudar("Ana");
 
+int resultado = somar( 5, 3);
+printf("O resultado da soma é: %d", resultado);
+
+int resultado2 = subtrair(5, 3);
+printf("\nO resultado da subtração é: %d", resultado2);
 
 return 0;
 }
