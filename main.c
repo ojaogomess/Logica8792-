466a096d6e1c09d72df324618e98e04d2b2843ba
ojaogomess <05796355031@senacrs.edu.br>
2026-09-22 21:15:11 -0300

    for retornoooooo++

--- main.c
+++ main.c
@@ -8,13 +8,11 @@ SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 int num[5] = {1, 2, 3, 4, 5};
 
-int i;
-
-for(i = 0; i < 5; i++){
-    printf("%d", num[i]);
+for(int i = 0; i < 5; i++){
+    printf("%d\n", num[i]);
 }
 
-printf("%d Retornoooooo\n", i);
+printf("\nRetornoooooo");
 
 return 0;
 }
