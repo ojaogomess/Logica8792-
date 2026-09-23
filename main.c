194bbe612ba3e0761cf879911c2c66604b61a2f8
ojaogomess <05796355031@senacrs.edu.br>
2026-09-22 21:21:41 -0300

    for relógio

--- main.c
+++ main.c
@@ -6,13 +6,21 @@ int main(){
 
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
-int num[5] = {1, 2, 3, 4, 5};
+int horas[12];
+int minutos[60];
 
-for(int i = 0; i < 5; i++){
-    printf("%d\n", num[i]);
+for(int i = 0; i < 12; i++){
+    horas[1] = i + 1;
 }
 
-printf("\nRetornoooooo");
+for(int j = 0; j < 60; j++){
+    minutos[j] = j;
+}
 
+for(int i = 0; i < 12; i++){
+    for(int j = 0; j < 60; j++){
+        printf("Hora: %2d:%02d\n", horas [1], minutos[j]);
+    }
+}
 return 0;
 }
