092646760763e2ec66feb9ebd028ccfa8780c040
ojaogomess <05796355031@senacrs.edu.br>
2026-09-23 21:28:24 -0300

    Contar vogais e usuario digita

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
 #include<stdio.h>
 #include<windows.h>
 
-void linha(int tamanho){
-    for (int i = 0; i < tamanho; i++){
-        printf("Agua");
+void contarVogais (char palavra[]){
+    int contador = 0;
+    for(int i = 0; i <strlen(palavra); i++){
+        char c = palavra[i];
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+        || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            contador++;
+        }//strlen calcula os caracteres de uma palavra
     }
+    printf("A palavra '%s', tem %d vogais \n", palavra, contador);
 }
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
+char palavra[20];
 
-linha(2000);
+printf("Digite uma palavra: ");
+scanf("%s", &palavra);
 
+contarVogais(palavra);
     
 return 0;
 
