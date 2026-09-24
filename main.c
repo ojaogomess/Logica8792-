d0fb7b4c90a2bfa18dfe7566840ab2b8454fb014
ojaogomess <05796355031@senacrs.edu.br>
2026-09-24 19:48:24 -0300

    while opcao diferente de 0, menu, depositar, consultar, sacar e sair

--- main.c
+++ main.c
@@ -5,15 +5,29 @@ int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
-int numero;
-    printf("Digite um número: ");
-    scanf("%d", &numero);
 
-    while(numero <= 0){
-        printf("Numero inválido! Digite novamente: ");
-        scanf("%d", &numero);
+int opcao = -1;
+
+while(opcao != 0){ //!= significa diferente 
+    printf("\nMenu: \n");
+    printf("1 - Depositar\n");
+    printf("2 - Sacar\n");
+    printf("3 - Consultar\n");
+    printf("0 - Sair\n");
+    printf("\nDigite uma opção:\n");
+    scanf("%d", &opcao);
+
+    switch(opcao){
+        case 1: printf("Voce escolheu Depositar\n"); break;
+        case 2: printf("Voce escolheu Sacar\n"); break;
+        case 3: printf("Voce escolheu Consultar\n"); break;
+        case 0: printf("Saindo...\n"); break;
+        default: printf("Opção Inválida.\n");
+
     }
-    printf("Número Váido: %d\n", numero);
+
+}
+
 return 0;
 
 }
