42ed647b7b2c562cf3f2bea340676cac94cd5ef2
ojaogomess <05796355031@senacrs.edu.br>
2026-09-24 20:17:21 -0300

    Digite numero para somar

--- main.c
+++ main.c
@@ -6,27 +6,22 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-int opcao = -1;
 
-while(opcao != 0){ //!= significa diferente 
-    printf("\nMenu: \n");
-    printf("1 - Depositar\n");
-    printf("2 - Sacar\n");
-    printf("3 - Consultar\n");
-    printf("0 - Sair\n");
-    printf("\nDigite uma opção:\n");
-    scanf("%d", &opcao);
+int numero, soma = 0;
+    
+    printf("Digite número para somar (0 encerra): ");
+    scanf("%d", &numero);
 
-    switch(opcao){
-        case 1: printf("Voce escolheu Depositar\n"); break;
-        case 2: printf("Voce escolheu Sacar\n"); break;
-        case 3: printf("Voce escolheu Consultar\n"); break;
-        case 0: printf("Saindo...\n"); break;
-        default: printf("Opção Inválida.\n");
-
+    while(numero != 0){
+        soma += numero;
+        scanf("%d", &numero);
     }
+    printf("Soma total = %d\n", soma);
+    
 
-}
+
+
+
 
 return 0;
 
