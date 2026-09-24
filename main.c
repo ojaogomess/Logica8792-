3c18200efbc9279acc4f422ed682acfdd2d568ff
ojaogomess <05796355031@senacrs.edu.br>
2026-09-24 19:05:40 -0300

    For com máximo e mínimio

--- main.c
+++ main.c
@@ -1,29 +1,33 @@
 #include<stdio.h>
 #include<windows.h>
 
-void contarVogais (char palavra[]){
-    int contador = 0;
-    for(int i = 0; i <strlen(palavra); i++){
-        char c = palavra[i];
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-        || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-            contador++;
-        }//strlen calcula os caracteres de uma palavra
+void maxMin(int v[], int tamanho){
+    int max = v[0], min = v[0];
+    for(int i = 1; i < tamanho; i++){
+        if(v[i] > max) max = v[i];
+        if(v[i] < min) min = v[i];
     }
-    printf("A palavra '%s', tem %d vogais \n", palavra, contador);
+    printf("Máximo: %d, Mínimo: %d\n", max, min);
+
 }
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-char palavra[20];
+int numeros[5];
 
-printf("Digite uma palavra: ");
-scanf("%s", &palavra);
+for(int i = 0; i < 5; i++){
+
+printf("Digite o numero %d: ", i + 1);
+
+scanf("%d", &numeros[i]);
+}
+
+maxMin(numeros, 5);
 
 
-contarVogais(palavra);
+
 
 
 
