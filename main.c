dc9da9fa6a5527b61a44ae74b779761e81ec193c
ojaogomess <05796355031@senacrs.edu.br>
2026-09-24 19:08:53 -0300

    Introdução ao While

--- main.c
+++ main.c
@@ -1,31 +1,17 @@
 #include<stdio.h>
 #include<windows.h>
 
-void maxMin(int v[], int tamanho){
-    int max = v[0], min = v[0];
-    for(int i = 1; i < tamanho; i++){
-        if(v[i] > max) max = v[i];
-        if(v[i] < min) min = v[i];
-    }
-    printf("Máximo: %d, Mínimo: %d\n", max, min);
-
-}
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-int numeros[5];
+    int i = 1;
 
-for(int i = 0; i < 5; i++){
-
-printf("Digite o numero %d: ", i + 1);
-
-scanf("%d", &numeros[i]);
-}
-
-maxMin(numeros, 5);
-
+    while(i <= 40){
+        printf("%d\n", i);
+        i++;
+    }
 
 
 
