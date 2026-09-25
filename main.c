6830d54b3a5301a2803c6d88b653711fe182105d
ojaogomess <05796355031@senacrs.edu.br>
2026-09-24 21:54:38 -0300

    Média

--- main.c
+++ main.c
@@ -6,16 +6,20 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-int n;
-long long fatorial = 1;
+    int n;
+    float valor, soma = 0;
 
-printf("Digite um número: ");
-scanf("%d", &n);
+    printf("Quantos números deseja digitar: ");
+    scanf("%d", &n);
 
-for(int i = 1; i <= n; i++){
-    fatorial *= i;
-}
-printf("Fatorial: %lld\n ", fatorial);
+    for(int i = 0; i < n; i++){
+        printf("Digite o número %d: ", i + 1);
+        scanf("%f", &valor);
+        soma += valor;
+
+    }
+
+printf("Média %.2f\n", soma / n);
 
 return 0;
 
