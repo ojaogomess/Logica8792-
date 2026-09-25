0b51479568e471b65bb23bd9a351e1673b231b99
ojaogomess <05796355031@senacrs.edu.br>
2026-09-25 19:31:24 -0300

    Primo ou não é primo com if,else, for

--- main.c
+++ main.c
@@ -6,17 +6,24 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int numero, contador = 0;
-
-    printf("Digite números (0 para parar): ");
-    scanf("%d", &numero);
-
-    while(numero != 0){
-        contador++;
-        scanf("%d", &numero);
+    int n, primo = 1;
+    printf("Digite um número: ");
+    scanf("%d", &n);
+    if(n < 2){
+        primo = 0;
+    }else{
+        for(int i = 2; i <= n / 2; i++){
+            if(n % i == 0){
+                primo = 0;
+                break;
+            }
+        }
     }
-    printf("Quantidade de números digitados: %d\n", contador);
-    
+    if(primo){
+        printf("%d é primo\n", n);
+    }else{
+        printf("%d não é primo\n", n);
+    }
 return 0;
 
 }
