44025a58737c02d42f7aa64e874e4d84ea1c75d5
ojaogomess <05796355031@senacrs.edu.br>
2026-09-24 21:02:37 -0300

    fatorial

--- main.c
+++ main.c
@@ -6,18 +6,16 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-int n, soma = 0;
+int n;
+long long fatorial = 1;
 
 printf("Digite um número: ");
 scanf("%d", &n);
 
-
-for (int i = 1; i <= n; i++){
-    soma += i;
+for(int i = 1; i <= n; i++){
+    fatorial *= i;
 }
-
-printf("O resultado da soma é: %d\n", soma);
-
+printf("Fatorial: %lld\n ", fatorial);
 
 return 0;
 
