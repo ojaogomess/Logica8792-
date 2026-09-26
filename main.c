cdd9453af0bd4e32a85b45d6151ce318067f08c7
ojaogomess <05796355031@senacrs.edu.br>
2026-09-25 21:27:59 -0300

    Média da turma com for

--- main.c
+++ main.c
@@ -14,12 +14,12 @@ float nota, soma = 0;
 printf("Quantos alunos tema  turma?: ");
 scanf("%d", &n);
 
-for(int i = 0; i < n; i++){
-    printf("Digite a nota do aluno %d: \n", i + 1);
-    scanf("%f", &nota);
-    soma += nota;
+for(int i = 0; i < n; i++){ //enquanto i menor que n (tamanho da turma), continue adicionando i
+    printf("Digite a nota do aluno %d: \n", i + 1); //digite a nota pertencente a cada aluno, a cada novo aluno para adicionar nota, adicione +1, para demonstrar aluno 1, 2, 3. 4...
+    scanf("%f", &nota); //escaneia a nota e a guarda na váriavel nota
+    soma += nota; // soma mais a nota e guarda valor
 }
-printf("Média da turma: %.2f\n", soma / n);
+printf("Média da turma: %.2f\n", soma / n); //após finalizar as notas com o tamanho da turma (n), mostrea média com float de duas casas decimais e soma dividido por tamanho da turma
 
 
 
